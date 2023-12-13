b25bd33eac8be5207a0f2fe3e15ac143aea909fd
YuriyAvengeR <yura0308@ukr.net>
2023-12-13 22:35:03 +0200

    1.0 Version

--- tray_version.py
+++ tray_version.py
@@ -4,12 +4,18 @@ from PIL import Image
 import pyperclip
 import time
 from threading import Thread
-import sys
 
 # Init variables
 copied_text = ''
 extend = "https://vxtwitter."
 link = ''
+stop_thread = False  # Flag
+
+# Stop thread
+def stop_program(icon, item=None):
+    global stop_thread
+    stop_thread = True
+    icon.stop()
 
 # Define the main function
 def on_clipboard_change(icon, item):
@@ -20,33 +26,32 @@ def on_clipboard_change(icon, item):
     if new_text.startswith("https://x.com") or new_text.startswith("https://twitter.com"):
         link = f"{extend}{new_text[10:]}"
         copied_text = new_text
-        print(f"Текст у буфері обміну: {copied_text}")
-        print(f"Текст після зміни: {link}")
         pyperclip.copy(link)
 
         # condition to exit the program
         if new_text == "exit_condition":
-            icon.stop()
-            sys.exit()
+            stop_program(icon)
 
 # Create a blank image for the icon
 image = Image.new('RGB', (128, 128), color=(255, 255, 255))
 
 # Create menu items
-menu = (item('', lambda icon, item: icon.stop()),)
-menu = (item('Stop and close', lambda icon, item: sys.exit()),)
+menu = (
+    item('Close', stop_program),
+)
 
 # Create the system tray icon
-icon = pystray.Icon("name", image, "Title", menu)
+icon = pystray.Icon("Twitter to Discord", image, "Twitter to Discord", menu)
 
 # Define the main function to run in a separate thread
 def main_thread():
-    while True:
+    while not stop_thread:  # stop thread
         on_clipboard_change(icon, None)
         time.sleep(1)
 
 # Run the main function in a separate thread
-Thread(target=main_thread).start()
+thread = Thread(target=main_thread)
+thread.start()
 
 # Run the system tray icon
 icon.run()
